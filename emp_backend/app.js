2bafc6f4cbb5c2e269c30e1e9d18a05986b775b2
Sourab19 <sourabberlin007@gmail.com>
2025-04-08 14:34:28 +0530

    server deploy2

--- emp_backend/app.js
+++ emp_backend/app.js
@@ -13,7 +13,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "/build")));
 
 app.get('/*',function(req,res){
-  res.sendFile(path.join(__dirname ,'/build/index.html'));
+  res.sendFile(path.join(__dirname ,'/src/index.html'));
 })
 
 const basicRoute1 = require("./routes/empRoutes");
